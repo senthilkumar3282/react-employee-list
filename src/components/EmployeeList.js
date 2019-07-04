2204e45959341a1de48f862f236eb426ec92430f
senthilkumar sugumar <senthilkumar3282@gmail.com>
2019-07-05 00:03:55 +0530

    Employee component
    Employee component

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import axios from 'axios';
+import '../styles/employee-panel.scss';
+
+class EmployeeList extends Component {
+	
+	render() {
+		return (	
+			<div className="section container">
+				<div className="row section-header">Employee List</div>
+				<div className="row section-body">
+				
+				</div>
+			</div>
+		);
+		
+	}
+}
+    
+export default EmployeeList;
